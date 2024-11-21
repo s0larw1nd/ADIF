28670d63fd7f44f1271fb00dff6f8db17eaedfbe
s0larw1nd <alixess13@gmail.com>
2024-11-21 16:19:16 +0300

    Added comments to the code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main() {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello world from " << name << std::endl;
-    return 0;
+    std::string name; // string to store name
+    std::cout << "Enter your name: "; // ask user for name 
+    std::cin >> name; // get user input
+    std::cout << "Hello world from " << name << std::endl; // print hello world
+    return 0; // end
 }
